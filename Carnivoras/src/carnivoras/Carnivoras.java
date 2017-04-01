32f22ee406652b83a56d5a1db2f8f3e7bda5f64d
Valentin1DAM <titi_malvinas@hotmail.com>
2017-04-02 00:48:02 +0200

    Merge origin/master
    Conflicts:
    	src/valentin1dam/Valentin1DAM.java

--- Carnivoras/src/carnivoras/Carnivoras.java
+++ Carnivoras/src/carnivoras/Carnivoras.java
@@ -16,6 +16,7 @@ public class Carnivoras {
      */
     public static void main(String[] args) {
        System.out.println("Dionaea Muscipula");
+       System.out.println("Nephentes");
     }
     
 }
