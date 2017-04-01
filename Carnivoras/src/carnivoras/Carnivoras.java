4e38f51479922e735628e8de4dd19d5d88d932d5
Valentin1DAM <titi_malvinas@hotmail.com>
2017-04-02 00:46:22 +0200

    Update Carnivoras.java

--- Carnivoras/src/carnivoras/Carnivoras.java
+++ Carnivoras/src/carnivoras/Carnivoras.java
@@ -15,7 +15,7 @@ public class Carnivoras {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.println("Dionaea Muscipula");
     }
     
 }
